621cef6a36056b72d63a989ff7201df4bd4300aa
unknown <snehiljain7@gmail.com>
2021-02-05 13:47:19 +0530

    first commit

---
+++ src/app/users.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import { CounterService } from "./counter.service";
+
+@Injectable()
+export class UserService{
+    activeUsers = ['Max', 'Anna'];
+    inactiveUsers = ['Chris', 'Manu'];
+
+    constructor(private counterService: CounterService){}
+    setToActive(id: number){
+        this.activeUsers.push(this.inactiveUsers[id]);
+        this.inactiveUsers.splice(id, 1);
+        this.counterService.incrementInactiveToActive();
+    }
+
+    setToInactive(id: number){
+        this.inactiveUsers.push(this.inactiveUsers[id]);
+        this.activeUsers.splice(id, 1);
+        this.counterService.incrementActiveToInactive();
+    }
+}
